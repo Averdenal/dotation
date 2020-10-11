2e14f578c4b0bb1c7c09419ed8670bfaedbf36f1
averdenal <amaury.y.verdenal@gmail.com>
2020-10-11 23:08:03 +0200

    home page begin

--- server/server.go
+++ server/server.go
@@ -1,12 +1,8 @@
 package server
 
 import (
-	"log"
-
 	"github.com/Averdenal/Dotation/Controller"
 	"github.com/gin-gonic/gin"
-
-	"html/template"
 )
 
 var r = gin.Default()
@@ -14,7 +10,9 @@ var r = gin.Default()
 //Server API
 func Server() {
 	r.Static("/assets", "./assets")
-	r.GET("/", demo)
+
+	r.GET("/", Controller.Home)
+	
 	demandes := r.Group("/Demandes")
 	{
 		//vos demandes
@@ -29,23 +27,5 @@ func Server() {
 		demandes.GET("/Service/:idService", Controller.NewDotation)
 
 	}
-
 	r.Run()
-}
-func demo(c *gin.Context) {
-	p := Page{"Titre de ma page", []string{"Item 1", "Item 2", "Item 3"}}
-
-	t := template.New("home page")
-
-	t = template.Must(t.ParseFiles("./Template/layout.tmpl", "./Template/content.tmpl"))
-
-	err := t.ExecuteTemplate(c.Writer, "layout", p)
-	if err != nil {
-		log.Fatalf("Template execution: %s", err)
-	}
-}
-
-type Page struct {
-	Title    string
-	Articles []string
 }
